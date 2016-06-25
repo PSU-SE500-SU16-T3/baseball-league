8b5e2221fd1077204ffd4379a8a5814b4eded0e4
Art Starovoyt <artstarovoyt@gmail.com>
2016-06-25 17:04:08 -0400

    additional beans for the database

---
+++ src/main/java/com/team3/business/models/Field.java
@@ -0,0 +1,26 @@
+package com.team3.business.models;
+
+public class Field {
+	private String fieldName;
+	private String fieldLocation;
+	private int fieldID;
+	
+	public String getFieldName() {
+		return fieldName;
+	}
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+	public String getFieldLocation() {
+		return fieldLocation;
+	}
+	public void setFieldLocation(String fieldLocation) {
+		this.fieldLocation = fieldLocation;
+	}
+	public int getFieldID() {
+		return fieldID;
+	}
+	public void setFieldID(int fieldID) {
+		this.fieldID = fieldID;
+	}
+}
---
+++ src/main/java/com/team3/business/models/RefereePlayer.java
@@ -0,0 +1,14 @@
+package com.team3.business.models;
+
+public class RefereePlayer extends Player {
+
+	private int refereePlayerID;
+	
+	public int getRefereePlayer() {
+		return refereePlayerID;
+	}
+	public void setRefereePlayer(int refereePlayerID) {
+		this.refereePlayerID = refereePlayerID;
+	}
+	
+}
