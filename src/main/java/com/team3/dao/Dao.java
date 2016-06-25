21137d44871b856b7975e68e8422320b97165000
Art Starovoyt <artstarovoyt@gmail.com>
2016-06-25 17:04:59 -0400

    DAO implementation

--- src/main/java/com/team3/dao/Dao.java
+++ src/main/java/com/team3/dao/Dao.java
@@ -1,7 +1,30 @@
 package com.team3.dao;
 
+import com.team3.business.models.Address;
+import com.team3.business.models.Division;
+import com.team3.business.models.Field;
+import com.team3.business.models.League;
+import com.team3.business.models.Payment;
+import com.team3.business.models.Phone;
+import com.team3.business.models.Player;
+import com.team3.business.models.RefereePlayer;
+import com.team3.business.models.Season;
+import com.team3.business.models.Team;
 import com.team3.business.models.User;
 
 public interface Dao {
+	
 	public void insertUser(User user);
+	public void insertPlayer(Player player);
+	public void insertPhone(Phone phone);
+	public void insertPayment(Payment payment);
+	public void insertLeague(League league);
+	public void insertSeason(Season season);
+	public void dropSeason(Season season);
+	public void insertDivision(Division division);
+	public void dropDivision(Division division);
+	public void insertTeam(Team team);
+	public void insertField(Field field);
+	public void insertRefereePlayer(RefereePlayer refereePlayer);
+	public void insertAddress(Address address);
 }
--- src/main/java/com/team3/dao/DaoImpl.java
+++ src/main/java/com/team3/dao/DaoImpl.java
@@ -3,6 +3,16 @@ package com.team3.dao;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Component;
 
+import com.team3.business.models.Address;
+import com.team3.business.models.Division;
+import com.team3.business.models.Field;
+import com.team3.business.models.League;
+import com.team3.business.models.Payment;
+import com.team3.business.models.Phone;
+import com.team3.business.models.Player;
+import com.team3.business.models.RefereePlayer;
+import com.team3.business.models.Season;
+import com.team3.business.models.Team;
 import com.team3.business.models.User;
 
 @Component("daoImpl")
@@ -14,6 +24,66 @@ public class DaoImpl extends JdbcDaoSupport implements Dao{
 					 
 		getJdbcTemplate().update(sql, new Object[] { user.getUsername(),
 				user.getUserPassword(), user.getUserEmail(), 345});
+		
+	}
+
+	public void insertPlayer(Player player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void insertPhone(Phone phone) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void insertPayment(Payment payment) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void insertLeague(League league) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void insertSeason(Season season) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void dropSeason(Season season) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void insertDivision(Division division) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void dropDivision(Division division) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void insertTeam(Team team) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void insertField(Field field) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void insertRefereePlayer(RefereePlayer refereePlayer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void insertAddress(Address address) {
+		// TODO Auto-generated method stub
 		
 	}
 
