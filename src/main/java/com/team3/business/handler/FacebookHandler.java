f1f7af67fca8d9363b5be7bf11ab7ac2ab7caad7
Brian <brian.p.hartel@gmail.com>
2016-07-24 19:33:22 -0700

    Minor Facebook changes for post

--- src/main/java/com/team3/business/handler/FacebookHandler.java
+++ src/main/java/com/team3/business/handler/FacebookHandler.java
@@ -1,9 +1,20 @@
 package com.team3.business.handler;
 
+import facebook4j.Facebook;
+import facebook4j.FacebookException;
+import facebook4j.FacebookFactory;
+import facebook4j.auth.AccessToken;
+
 public class FacebookHandler implements ISocialMediaHandler {
 
 	public void PostStatus(String Post) {
-		// TODO Auto-generated method stub
+		Facebook facebook = getfacebook();
+		try {
+			facebook.postStatusMessage(Post);
+		} catch (FacebookException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
@@ -11,6 +22,14 @@ public class FacebookHandler implements ISocialMediaHandler {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	private Facebook getfacebook(){
+		Facebook facebook = new FacebookFactory().getInstance();
+		facebook.setOAuthAppId("270872636624990", "405765261426f0d5f39ec51dd3ed4953");
+		String accessTokenString = "EAACEdEose0cBAH92UzpMXvODUhMoHmyYHmFyCVTT88oRVZAfT3gxZCc7A3CQBLCEQTYN3rfZC1AAV9G5gUmpTMJe8E0HUhdNAvyOscGZAIx3aksFIZC7nZAcGG9XBv3UqK4MXEASIsUZBlhYWe15fCMZCElSxo8kiOFCyUxbzIIWnAZDZD";
+		AccessToken at = new AccessToken(accessTokenString);
+		facebook.setOAuthAccessToken(at);
+		return facebook;
+	}
 
 }
